e2b3b8ad25671fc90d16b11ddddad5ff4378d477
frimpongopoku <pongofrimi@gmail.com>
2021-02-02 02:04:09 +0400

    Initial Commit

---
+++ Shared/donkomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  donkomiApp.swift
+//  Shared
+//
+//  Created by frimpongOpokuAgyemang on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct donkomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
